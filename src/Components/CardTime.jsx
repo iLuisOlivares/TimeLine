f3655d48017a0199b44576d73cbe74eaab820812
Luis Olivares <Luissolivaresp@gmail.com>
2022-06-09 17:20:45 -0500

    09-06-2022

--- src/Components/CardTime.jsx
+++ src/Components/CardTime.jsx
@@ -3,11 +3,12 @@ import "../css/cardStyle.css";
 
 export const CardTime = () => {
   return (
-	<div class="evento">
-			<div class="foto" data-aos="fade-up">
+	<div className="evento">
+			<div className="foto" data-aos="fade-up">
 				<img src="https://img1.ak.crunchyroll.com/i/spire2/a6e36e575f9d9d38d1cf40d6769980a31651739960_main.jpg" alt=""/>
+				<p>9. 01. 2021.</p>
 			</div>
-			<h3 class="fecha">9 de Enero de 2021</h3>
+			<h3 className="fecha">9 de Enero de 2021</h3>
 		</div>
   )
 }
